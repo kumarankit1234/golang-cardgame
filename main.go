fe8516c53c48978e5b8753be556a9d7ec26fdaa7
Ankit Sinha <ankit.sin@media.net>
2018-10-08 15:39:37 +0530

    Type conversion in go

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	cards := newDeck()
-	hand, remaingCards := deal(cards, 5)
+	// cards := newDeck()
+	// hand, remaingCards := deal(cards, 5)
 
-	hand.print()
-	fmt.Println()
-	remaingCards.print()
+	// hand.print()
+	// fmt.Println()
+	// remaingCards.print()
+
+	greeting := "Hi there"
+
+	fmt.Println([]byte(greeting))
 
 }
