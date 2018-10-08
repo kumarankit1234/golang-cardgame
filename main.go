3ded33bedd0f218ad22dac5635361b9a231fb5b4
Ankit Sinha <ankit.sin@media.net>
2018-10-08 13:48:35 +0530

    Create custom data type and add receiver method

--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	cards := getCard()
-	fmt.Println(cards)
 
+	cards := deck{"Ace of diamond", getCard()}
+	cards.print()
 }
 
 func getCard() string {
