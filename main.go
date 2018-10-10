12828c44ae971d2ccf193eff3a8ff2997dbb36dc
Ankit Sinha <ankit.sin@media.net>
2018-10-10 09:38:20 +0530

    Use random number generator with seed to shuffle cards

--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
 
 	cards := newDeckFromFile("/Users/ankit.sin/my-cards.txt")
-	fmt.Println(cards.toString())
+	cards.shuffle()
+	cards.print()
 
 }
